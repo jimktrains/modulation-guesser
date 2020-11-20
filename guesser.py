121282753d55cedfa3a432304076d83eee9bc0b7
Jim Keener <jim@jimkeener.com>
2020-11-20 10:43:02 -0500

    adding a first pass at the guesser

---
+++ guesser.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
+# Yes, Pandas is overkill for this.
+from pandas import read_csv
+from numpy import amin, mean, sqrt, square, int32
+
+reference = read_csv('data.tsv', sep='\t')
+
+parser = ArgumentParser(
+    description='Guess a modulation based on the given parameters'
+)
+parser.add_argument('--center_freq', '-c', metavar='CENTER_FREQ', type=int,
+                    help='center frequency')
+parser.add_argument('--bandwidth', '-b', metavar='BANDWIDTH', type=int,
+                    help='bandwidth')
+parser.add_argument('--rms', '-r', metavar='RMS', type=float,
+                    help='RMS of the signal')
+for i in range(-3,4):
+    parser.add_argument(f"--spectral-bin-{i:+}", f"-{i+3}", 
+                        metavar=f"SPECTRAL_BIN_{i:+}", type=float,
+                        help=f"Spectral bin {i:+}",
+                        dest=f"spectral_bin_{i+3}")
+
+args = parser.parse_args()
+
+possible_modulations = reference.loc[reference['Bandwidth (50Hz Bucket)'] == args.bandwidth]
+
+measured_bins = [
+        args.spectral_bin_0,
+        args.spectral_bin_1,
+        args.spectral_bin_2,
+        args.spectral_bin_3,
+        args.spectral_bin_4,
+        args.spectral_bin_5,
+        args.spectral_bin_6,
+]
+ref_bins = possible_modulations.loc[:,'Freq Bin -3':'Freq Bin +3']
+delta_bins = ref_bins - measured_bins
+delta_rms = sqrt(mean(square(delta_bins.T)))
+best_modulation_idx = delta_rms.idxmin()
+
+print(reference.loc[best_modulation_idx, 'Mode'])
